0d5ddc3960869163d28d04446584ae05cb9bbb78
James <jr39.dev@gmail.com>
2018-05-21 22:22:00 +0200

    documentation + error handling example

--- doc.go
+++ doc.go
@@ -1,27 +1,4 @@
-package semaphore
-
 /*
-A semaphore implementation based on https://pauladamsmith.com/blog/2016/04/max-clients-go-net-http.html
-
-Example
-
-```
-func main() {
-	s := NewSemaphore(4)
-	for i := 0; i < 22; i++ {
-		s.Add()
-		go func(i int) {
-			defer func() {
-				s.Done()
-				fmt.Printf("finished %d\n", i)
-			}()
-			fmt.Printf("running: %d\n", i)
-			time.Sleep(time.Second * time.Duration(i))
-		}(i)
-	}
-	s.Wait()
-}
-
-```
-
+Package semaphore provides a semaphore/queuing implementation based on https://pauladamsmith.com/blog/2016/04/max-clients-go-net-http.html
 */
+package semaphore
